c49bc53d23ed23d0187a03f97f5d5732ce9a85ff
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-26 17:37:12 -0300

    refactor

--- src/menu/ProductMenu.java
+++ src/menu/ProductMenu.java
@@ -4,9 +4,9 @@ public class ProductMenu {
 	public static void show () {
 		System.out.println("\n|------------------|");
 		System.out.println("| Chose an option: |");
-		System.out.println("| [1]Save  [4]Read |");
-		System.out.println("| [2]Edit  [5]List |");
-		System.out.println("| [3]Drop  [6]Exit |");
+		System.out.println("| [1]Save  [4]List |");
+		System.out.println("| [2]Edit  [5]Drop |");
+		System.out.println("| [3]Exit          |");
 		System.out.println("|------------------|");
 		System.out.print("↳ ");
 	}
