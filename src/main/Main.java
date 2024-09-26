69432001eecbfebc9cfd223f0409d89dfcf0b7a5
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-26 17:29:41 -0300

    save implementation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,33 @@
 package main;
 
+import java.util.Scanner;
+import menu.SaveMenu;
+import menu.ProductMenu;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner in = new Scanner(System.in);
+
+		while (true) {
+			ProductMenu.show();
+			int option = in.nextInt();
+
+			switch (option) {
+				case 1:
+					SaveMenu.show();
+					break;
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					System.out.println("\nExiting...");
+					in.close();
+					return;
+				default:
+					System.out.println("\nEnter a valid option");
+					break;
+			}
+		}
 	}
 }
