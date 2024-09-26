3eda3e5a4161ab9073758dd92bffb22576f3c3fc
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-26 17:24:08 -0300

    Menus for startup and save method

---
+++ src/menu/ProductMenu.java
@@ -0,0 +1,13 @@
+package menu;
+
+public class ProductMenu {
+	public static void show () {
+		System.out.println("\n|------------------|");
+		System.out.println("| Chose an option: |");
+		System.out.println("| [1]Save  [4]Read |");
+		System.out.println("| [2]Edit  [5]List |");
+		System.out.println("| [3]Drop  [6]Exit |");
+		System.out.println("|------------------|");
+		System.out.print("↳ ");
+	}
+}
---
+++ src/menu/SaveMenu.java
@@ -0,0 +1,24 @@
+package menu;
+
+import java.util.Scanner;
+
+import dao.ProductDAO;
+import entity.Product;
+
+public class SaveMenu {
+	public static void show () {
+		Scanner in = new Scanner(System.in);
+		System.out.print("\nEnter the quantity: ");
+		int quantity = in.nextInt();
+		System.out.print("Enter the SKU: ");
+		String sku = in.next();
+		System.out.print("Enter the description: ");
+		String description = in.next();
+		System.out.print("Enter the price: ");
+		float price = in.nextFloat();
+		System.out.print("Enter the max discount: ");
+		float maxDiscount = in.nextFloat();
+		Product product = new Product(quantity, sku, description, price, maxDiscount);
+		ProductDAO.Create(product);
+	}
+}
