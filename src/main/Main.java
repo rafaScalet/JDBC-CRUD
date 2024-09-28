f179ddc5da5a2a349e6c75fd5e672496cf596809
rafaScalet <rafaelscalet2018@gmail.com>
2024-09-28 12:41:27 -0300

    refactor

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,7 @@
 package main;
 
 import java.util.Scanner;
-import menu.SaveMenu;
-import menu.UpdateMenu;
-import menu.ListMenu;
-import menu.ProductMenu;
+import menu.*;
 
 public class Main {
 	public static void main(String[] args) {
